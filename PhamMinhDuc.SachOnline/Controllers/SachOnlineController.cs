c06324dcda34b5b5814530c2dc0c6320f749afef
HiSapril <2324802010207@student.tdmu.edu.vn>
2025-09-17 19:45:22 +0700

    Thêm code ban đầu

---
+++ PhamMinhDuc.SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhamMinhDuc.SachOnline.Models;
+
+namespace PhamMinhDuc.SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // Add this line to define the 'data' context
+        private SachOnlineEntities data = new SachOnlineEntities();
+
+        // GET: SachOnline
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            //Lay 6 quyen sach moi
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd;
+            return PartialView(listChuDe);
+        }
+        private List<SACH> LaySachBanNhieu(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.SoLuongBan).Take(count).ToList();
+        }
+        public ActionResult SachBanNhieuPartial()
+        {
+            var listSachBanNhieu = LaySachBanNhieu(6);
+            return PartialView(listSachBanNhieu);
+        }
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNXB = from nxb in data.NHAXUATBANs select nxb;
+            return PartialView(listNXB);
+        }
+        public ActionResult FooterPartial()
+        {
+            return PartialView();
+        }
+
+    }
+}
